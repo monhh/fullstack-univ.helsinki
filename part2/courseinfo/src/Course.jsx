89e4453a04cf0f71b7c0e72e19a32a9a4660fa68
Mon Pampín <mon.medialab@gmail.com>
2024-06-08 18:57:44 +0200

    part2 courseinfo exercises 2.1 to 2.5

---
+++ part2/courseinfo/src/Course.jsx
@@ -0,0 +1,37 @@
+//component responsible for formatting a single course called Course
+
+const Header = ({name}) => {
+    return <h2>{name}</h2>
+}
+const Part = ({part,exercises}) => {
+    return (
+        <p>
+            {part} {exercises}
+        </p>
+    )
+}
+const Content = ({parts}) => {
+    return (
+        <div>
+            {parts.map(part => 
+                <Part key={part.id} part={part.name} exercises={part.exercises} />)}
+        </div>
+    )
+}
+const Total = ({parts}) => {
+
+    const sumOfExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+    return <p style={{fontWeight: 'bold'}}>total of {sumOfExercises} exercises</p>
+  }
+const Course = ({ course }) => {
+
+    return (
+        <div>
+            <Header name={course.name} />           
+            <Content parts={course.parts} />
+            <Total parts={course.parts} /> 
+        </div>
+    )
+}
+export default Course
